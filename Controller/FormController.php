b3542aefb66666c8045fcbada049497d355675b8
Ubuntu <ubuntu@ip-172-31-34-86.ap-northeast-1.compute.internal>
2019-12-13 20:54:40 +0000

    wip regist form_values

--- Controller/FormController.php
+++ Controller/FormController.php
@@ -71,35 +71,74 @@ class FormController
         $postParams = $request->getParsedBody();
         var_dump($postParams);
         
-        $sql = "SELECT * FROM form_items ";
-        $stmt = $this->app->db->query($sql);
-        $form_items = [];
-        
-        while($row = $stmt->fetch()) {
-            $form_items[] = $row;
-        }
         
         // form_submitsに登録してid取得
         $sql = "INSERT INTO `form_submits` (
-                    `created_at`, 
-                    `updated_at`) 
+                    `created_at`, `updated_at`) 
                     VALUES (:created_at, :updated_at);";
         $stmt = $this->app->db->prepare($sql);
-        
+
         $this->app->db->beginTransaction();
         try {
             $stmt->bindValue(':created_at', date("Y-m-d H:i:s"), PDO::PARAM_STR);
             $stmt->bindValue(':updated_at', date("Y-m-d H:i:s"), PDO::PARAM_STR);
             $res = $stmt->execute();
             
-            $last_id =  $this->app->db->lastInsertId('id');
-            var_dump($last_id);
+            $last_id = $this->app->db->lastInsertId('id');
+            // var_dump($last_id);
+            
+            // form_valueに登録
+            foreach ($postParams as $key => $value) {
+                
+                $fi_sql = "SELECT label_name, schema_name, input_type FROM form_items where schema_name = :key ";
+                $fi_stmt = $this->app->db->prepare($fi_sql);
+                $fi_stmt = bindValue(':key', $key, PDO::PARAM_STR);
+                $fi_stmt->execute();
+                $fi_result = $fi_stmt->fetch();
+                
+                var_dump($fi_result);
+                
+
+                $fv_sql = "INSERT INTO `form_values` (
+                        `submit_id`, ,`colmun_name`, `text`, `textarea`, `int`, `datetime`, `bool`) 
+                        VALUES (:submit_id, :colmun_name, :text, :textarea, :int, :datetime, :bool);";
+                $fv_stmt = $this->app->db->prepare($fv_sql);
+                        
+                $fv_stmt->bindValue(':submit_id', intVal($last_id), PDO::PARAM_INT);
+                $fv_stmt->bindValue(':colmun_name', $key, PDO::PARAM_STR);
+                
+                switch($fi_result['input_type']) {
+                    
+                    case "text":
+                    case "textarea":
+                        $fv_stmt->bindValue(":${fi_result['input_type']}", $value, PDO::PARAM_STR);
+                        break;
+                    
+                    case "int":
+                        $fv_stmt->bindValue(':int', intVal($value), PDO::PARAM_INT);
+                        break;
+                        
+                    case "datetime":
+                        $fv_stmt->bindValue(':datetime', date("Y-m-d H:i:s", $value), PDO::PARAM_STR);
+                        break;
+                        
+                    case "bool":
+                        $fv_stmt->bindValue(':bool', $value, PDO::PARAM_BOOL);
+                        break;
+                        
+                    default:
+                        break;
+                }
+                
+                $fv_res = $fv_stmt->execute();
+                
+            }
         } catch (Exception $e) {
             $this->app->db->rollBack();
             throw $e;
         }
         
-        // form_valueに登録
+        
         
         // return $this->app->view->render($response, 'form/complete.html');
     }
@@ -112,7 +151,6 @@ class FormController
         while($row = $stmt->fetch()) {
             $form_items[] = $row;
         }
-        
         return $form_items;
     }
 }
